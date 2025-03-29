bda3349daa38bdb2b1b8e33cca6adb3ddaf62f65
Ayush <vaibhavvf10@gmail.com>
2025-03-29 18:10:48 +0530

    Initial Project Setup

---
+++ src/main/java/com/spring/securityvault/SpringSecurityWithVault/SpringSecurityWithVaultApplication.java
@@ -0,0 +1,13 @@
+package com.spring.securityvault.SpringSecurityWithVault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWithVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/securityvault/SpringSecurityWithVault/SpringSecurityWithVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.securityvault.SpringSecurityWithVault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWithVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
